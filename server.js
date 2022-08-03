ffa94b3593381206b2764803cd9227810b28063c
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-03 15:57:54 -0500

    console logging the correct port

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ const sess = {
   app.use(routes)
   
   sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening on Port ' + PORT));
   });
