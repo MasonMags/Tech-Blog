f756953d1c5cd401d90c7ca82a99f142781f9d58
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-03 15:33:32 -0500

    set up middleware in server file

--- server.js
+++ server.js
@@ -7,3 +7,26 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  }
+  
+  app.use(session(sess));
+  
+  const hbs = exphbs.create({})
+  
+  app.engine("handlebars",hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}));
+  app.use(express.static(path.join(__dirname,  'public'))); 
+  
+  app.use(routes)
+  
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
