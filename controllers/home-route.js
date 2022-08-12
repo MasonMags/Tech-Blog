dcd62d255633730e8280e57474cdcb4db1c436d6
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-11 19:12:35 -0500

    homeroute finds post data; renders homepage

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
-// const User = require('../models/User')
+const Post = require('../models/Post')
 // const Book = require('../models/Book');
 const withAuth = require('../utils/auth')
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  try {
+    const postData = await Post.findAll({});
+    const post = postData.map((postData) => 
+    postData.get({ plain:true })
+    );
+    console.log(post)
+    
+    res.render('homepage', {
+      post, logged_in: req.session.logged_in
+    });
+  } 
+  catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router
