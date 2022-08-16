03edcef2872963889904415f4142a73b1ad71633
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-16 18:21:10 -0500

    added post route to create new user

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -44,3 +44,26 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err)
       }
 });
+
+// creating a new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+        console.log(userData);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+});
+
