701fb721f87131f0aaae0c88b11ea7612d9e0bff
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-16 18:19:05 -0500

    added get route for one user

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -14,4 +14,33 @@ router.get('/', async (req, res) => {
     console.log(err)
     res.status(500).json(err)
   }
-})
+});
+
+router.get('/:id', (req, res) => {
+ try {
+    const userData = User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_text', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                  model: Post,
+                  attributes: ['title']
+                }
+            }
+        ]
+    })
+    res.status(200).json(userData);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+      }
+});
