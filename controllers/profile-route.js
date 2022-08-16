c68a0c592f20846acdcf12d8d35c91debf20c2f3
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-15 21:22:19 -0500

    route for finding all of a user's posts

--- controllers/profile-route.js
+++ controllers/profile-route.js
@@ -3,3 +3,38 @@ const Post = require('../models/Post');
 const User = require('../models/User');
 const Comment = require('../models/Comment');
 
+router.get('/', withAuth, async (req, res) => {
+   try {
+   const postData = await Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes:  ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                  }
+            },
+            {
+                model: User,
+                attributes: ['username']
+              }
+        ]
+    })
+    const posts = postData.get({ plain: true })
+    res.render('profile', {posts, logged_in: true })
+} 
+catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }  
+})
