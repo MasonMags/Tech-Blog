11a37353e76771a20ef44d9abb6232bd5d480188
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-16 18:11:42 -0500

    added get route for users

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -2,3 +2,16 @@ const router = require('express').Router();
 const User = require('../../models/User');
 const Post = require('../../models/Post');
 const Comment = require('../../models/Comment');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+ try{
+    const userData = await User.findAll({
+        attributes: { exclude: ['password']}
+    })
+    res.status(200).json(userData);
+} catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
