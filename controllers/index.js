cee7f55048021b0111a20fa62b0a01e2e99de8c1
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-16 19:34:24 -0500

    router using api and profile routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-route')
+const profileRoutes = require('./profile-route')
 
-// router.use('/api', apiRoutes);
-router.use("/", homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes)
+router.use('/', homeRoutes);
+
 module.exports = router;
