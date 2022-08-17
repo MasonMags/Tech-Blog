c558523e61c9253b17b279b205c44b0925e0f9a5
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-16 19:28:02 -0500

    added put route to update user by id

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -115,4 +115,26 @@ router.post('/logout', withAuth, (req, res) => {
         console.log("could not log out")
         res.status(404).end();
     }
-})
+});
+
+// updating a user by id
+router.put('/:id', withAuth, (req, res) => {
+ try {
+    const userData = await User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    if (!userData[0]) {
+        res.status(404).json({ message: 'No user with this id!' });
+        return;
+      }
+      res.status(200).json(userData);
+ } catch(err) {
+    console.log(err)
+    res.status(500).json(err);
+}
+});
+
+module.exports = router;
