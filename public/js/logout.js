ffc6e066acf6d21ec4f721f6870b97d70cf65410
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-17 19:08:52 -0500

    added logout logic

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logout = async () => {
+    console.log('logout.js file hit')
+      const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+    
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        console.log("failed to log out")
+        alert('Failed to log out.');
+      }
+    };
+    
+    document.querySelector('#logout').addEventListener('click', logout);
