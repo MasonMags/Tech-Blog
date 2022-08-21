47874a087bb007243c9b2b88234bf4b2e7af27c0
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-21 16:44:56 -0500

    passing helper function for date format to hbs

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const routes = require('./controllers/index.js')
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+const helpers = require('./utils/helpers')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,7 +18,7 @@ const sess = {
   
   app.use(session(sess));
   
-  const hbs = exphbs.create({})
+  const hbs = exphbs.create({helpers})
   
   app.engine("handlebars",hbs.engine);
   app.set('view engine', 'handlebars');
