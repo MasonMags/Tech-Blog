840520a8fd037db643d123d69bb851841a66d135
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-20 23:46:21 -0500

    cleaning up extra code

--- controllers/profile-route.js
+++ controllers/profile-route.js
@@ -4,42 +4,6 @@ const User = require('../models/User');
 const Comment = require('../models/Comment');
 const withAuth = require('../utils/auth.js');
 
-// router.get('/', withAuth, async (req, res) => {
-//    try {
-//    const postData = await Post.findAll({
-//         where: {
-//             user_id: req.session.user_id
-//         },
-//         attributes: [
-//             'id',
-//             'title',
-//             'post_text',
-//             'created_at'
-//         ],
-//         include: [
-//             {
-//                 model: Comment,
-//                 attributes:  ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                 include: {
-//                     model: User,
-//                     attributes: ['username']
-//                   }
-//             },
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//               }
-//         ]
-//     })
-//     const posts = postData.get({ plain: true })
-//     res.render('profile', {posts, logged_in: true })
-// } 
-// catch (err) {
-//     console.log(err)
-//     res.status(500).json(err)
-//   }  
-// });
-
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
       where: {
@@ -70,7 +34,7 @@ router.get('/', withAuth, (req, res) => {
       .then(dbPostData => {
         // serialize data before passing to template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('profile', { posts, loggedIn: true });
+        res.render('profile', { posts, logged_In: true });
       })
       .catch(err => {
         console.log(err);
@@ -106,7 +70,7 @@ router.get('/edit/:id', withAuth, async (req, res) =>{
       ]
     })
     const editPost = postData.get({ plain: true })
-    res.render('edit-post', { editPost, loggedIn: req.session.loggedIn});
+    res.render('edit-post', { editPost, logged_In: req.session.logged_In});
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
