797adf92d2cef18c6ba3ee4c1479e0b674d78a62
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-23 11:26:44 -0500

    trying old config again

--- config/connection.js
+++ config/connection.js
@@ -12,18 +12,14 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
-// let sequelize;
-
-//     // when deployed on Heroku
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   // localhost
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
+    // const sequelize = process.env.JAWSDB_URL
+    // ? new Sequelize(process.env.JAWSDB_URL)
+    // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    //     host: '127.0.0.1',
+    //     dialect: 'mysql',
+    //     dialectOptions: {
+    //       decimalNumbers: true,
+    //     },
+    //   });
 
 module.exports = sequelize;
